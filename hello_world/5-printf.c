987e361a3c0c5f038ee0018372c9a8cea2a62e44
mauricio <imcorreamauricio@gmail.com>
2022-10-01 11:27:21 -0700

    task5 printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - print a array
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - print a array
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
