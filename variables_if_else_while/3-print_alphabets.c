822a243f4fa98e4ad16a0481d971ce9f4e8b7b63
mauricio <imcorreamauricio@gmail.com>
2022-10-01 13:14:43 -0700

    task 2 alph

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *  main - Displays the whole alphabet in lowercase
+ *
+ * Return: 0 in Succes
+ *
+ * */
+
+int main(void)
+{
+		char ch = 'a';
+			char cj = 'A';
+
+	while (ch <= 'z')
+	{
+	putchar(ch)
+	(ch++);
+}
+	while (cj <= 'Z')
+{
+	putchar(cj);
+	(cj++);
+}
+	putchar('\n');
+	return (0);
+}
