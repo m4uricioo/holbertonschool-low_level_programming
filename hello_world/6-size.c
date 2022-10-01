560cb4d99b85705b830c6db3c7db049c743ee359
mauricio <imcorreamauricio@gmail.com>
2022-10-01 12:43:56 -0700

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,22 +3,13 @@
  *	main - print byres variables
  *	Return: Always 0 (Succsess)
  */
-
 	int main(void)
 {
-	char character;
-	int inter;
-	long int long_inter;
-	long long int inter_64;
-	float simple_decimal;
-		char character;
-		long int long_inter;
-		long long int inter_64;
-		float simple_decimal;
-		printf("Size of a char: %zu byte(s)\n", sizeof(character));
-		printf("Size of an int: %zu byte(s)\n", sizeof(inter));
-		printf("Size of a long int: %zu byte(s)\n", sizeof(long_inter));
-		printf("Size of a long long int: %zu byte(s)\n", sizeof(inter_64));
-		printf("Size of a float: %zu byte(s)\n", sizeof(simple_decimal));
-		return (0);
+	
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
+		printf("Size of an int: %d byte(s)\n", sizeof(int));
+		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+		printf("Size of a float: %d byte(s)\n", sizeof(float));
+				return (0);
 }
