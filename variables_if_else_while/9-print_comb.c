c0a59ad548b42714b3ad01d93aea5d2ff9bdaca2
mauricio <imcorreamauricio@gmail.com>
2022-10-01 17:44:00 -0700

    task print comb9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	for (; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
