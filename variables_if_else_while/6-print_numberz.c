8d2e6750e4139b545432114875891b0fa0b98112
mauricio <imcorreamauricio@gmail.com>
2022-10-01 17:15:27 -0700

    task print numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all single numbers of base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int nume = 0;
+
+	for (; num < 10; num++)
+		putchar((num % 10 + '0'); /* base 10 number from 9 to 9 , 0 is not a decimal number*/
+				putchar('\n');
+				return (0);
+				}
