e1645022b32c63f465b26eb06e4eeb8628f704b5
mauricio <imcorreamauricio@gmail.com>
2022-10-03 08:35:00 -0700

    task zero

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,16 @@
-main 
+/**
+ * main - print a characters
+ * Return: 0 
+ **/
+int main(void)
+{
+		char *str = "_putchar";
+		while (*str)
+	{
+	  _putchar(*str);
+		str++;
+		}
+	_putchar('\n');
+	return (0);
+}
+
