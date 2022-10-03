b850e456eb050294feb35ebbf6f0402e671c7d83
mauricio <imcorreamauricio@gmail.com>
2022-10-02 17:48:59 -0700

    refresh

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- *  main - print a characters
- *   Return: 0
+ *  main - prints the word _putchar followd by a new line
+ *
+ *  Return: 0
  */
 
 int main(void)
 {
-	char *str = "_putchar";
-
-	while (*str)
-	{
-		_putchar(*str);
-		str++;
-	}
+	_putchar(95);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
