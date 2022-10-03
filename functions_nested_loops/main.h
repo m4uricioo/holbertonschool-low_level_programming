1ac265357bbd2ff8ed29676b2a161c6359bc32a6
mauricio <imcorreamauricio@gmail.com>
2022-10-03 09:18:15 -0700

    o

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,16 +1,34 @@
-/**
- * main - print a characters
- * Return: 0 
- **/
-int main(void)
-{
-		char *str = "_putchar";
-		while (*str)
-	{
-	  _putchar(*str);
-		str++;
-		}
-	_putchar('\n');
-	return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+/*libraries*/
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+/*prototypes*/
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+#endif /*main.h*/
 
