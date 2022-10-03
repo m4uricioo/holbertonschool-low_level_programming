387a3ff4cbd4bd4e69a587e0e917270c950103d3
mauricio <imcorreamauricio@gmail.com>
2022-10-02 17:37:41 -0700

    0 putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  main - print a characters
+ *   Return: 0
+ */
+
+int main(void)
+{
+	char *str = "_putchar";
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+	return (0);
+}
