8ef327757a85a02ea90204254528720b9049a1cf
mauricio <imcorreamauricio@gmail.com>
2022-10-03 19:15:58 -0700

    task 10 ad

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
-iinclude "main.h"
+#include "main.h"
 /**
  *  * add - adds two integers
  *   *@num1: number received
  *    *@num2: number received
  *     * Return: the result
- */
+*/
 int add(int num1, int num2)
 {
 		return (num1 + num2);
