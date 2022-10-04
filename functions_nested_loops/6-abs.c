56ef1f7d680b8578388e627deb2d2c9e18d2bf19
mauricio <imcorreamauricio@gmail.com>
2022-10-03 17:11:15 -0700

    6 abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: is a integer number resived
+ * Return: less than 0 absolute value,greater than 0 the number, otherwise 0
+ */
+int _abs(int i)
+{
+		if (i < 0)
+{
+		i = (-1) * i;
+		return (i);
+}
