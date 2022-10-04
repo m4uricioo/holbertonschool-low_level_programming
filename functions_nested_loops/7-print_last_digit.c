680494154512fb0366684d29f29a1446ee9c428a
mauricio <imcorreamauricio@gmail.com>
2022-10-03 17:22:52 -0700

    up

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  print_last_digit - prints the last digit of a number
+ *  @num: number received
+ *   Return: value of the last digit
+ */
+int print_last_digit(int num)
+{
+		int last_d = num % 10;
+			int ASC;
+
+				if (last_d < 0)
+						{
+									last_d = -last_d;
+										}
+					_putchar(ASC = '0' + last_d);	/*converts to its ASCII value*/
+		return (last_d);
+}
