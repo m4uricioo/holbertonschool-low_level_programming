0e8b10fcd36cbc0680733ca59157a078d44c2ac8
mauricio <imcorreamauricio@gmail.com>
2022-10-05 12:42:26 -0700

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * _isdigit - returns 1 if c is a digit
+ *   * @c: digit
+ *    * Return: 0
+ *     */
+
+int _isdigit(int c)
+{
+		if (c < 48 || c > 57)
+				{
+							return (0);
+								}
+			else
+					{
+								return (1);
+									}
+}
