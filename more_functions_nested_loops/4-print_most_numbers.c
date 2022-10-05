ec8a784197bc1b4c52c86841c70575c200381675
mauricio <imcorreamauricio@gmail.com>
2022-10-05 13:11:33 -0700

    task4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  *  * print_most_numbers - prints the numbers, from 0 to 9
- *   */
+ */
 void print_most_numbers(void)
 {
 	int num = '0';
 
 	for (; num <= '9'; num++)
 	{
-	if (num != '2' && num != '4')	
+	if (num != '2' && num != '4')
 	{
 	_putchar(num);
 														}
