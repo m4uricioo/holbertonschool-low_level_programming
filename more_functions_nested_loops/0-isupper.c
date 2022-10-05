307f85a264b9a40171d902702c1b401c50f2b4ae
mauricio <imcorreamauricio@gmail.com>
2022-10-05 12:38:05 -0700

    task more loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _isupper - returns 1 if c is upper case
+ *   * @c: character
+ *  Return: 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c < 65 || c > 90)
+	{
+	return (0);
+	}
+	else
+	{
+	return (1);
+	}
+}
