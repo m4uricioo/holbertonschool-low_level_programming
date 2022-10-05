09ba2c5346273d7d6780b80668dd8931aed0b5f7
mauricio <imcorreamauricio@gmail.com>
2022-10-05 12:44:58 -0700

    main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,11 +1,30 @@
-int _isupper(int c);
-void print_triangle(int size);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
+#ifndef MAIN_H
+#define MAIN_H
+/*libraries*/
+#include <stdio.h>
+#include <unistd.h>
+/*prototypes*/
+
 int _putchar(char c);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
 void print_most_numbers(void);
+
 void more_numbers(void);
+
 void print_line(int n);
+
 void print_diagonal(int n);
-void print_square(int siz
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+#endif /*main.h*/
