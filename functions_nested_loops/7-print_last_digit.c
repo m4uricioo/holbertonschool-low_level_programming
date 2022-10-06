5291e31d37ce1bc1fcd81a09e2d503c94e808db3
mauricio <imcorreamauricio@gmail.com>
2022-10-06 11:11:11 -0700

    main.h

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,9 @@ int print_last_digit(int num)
 		int last_d = num % 10;
 			int ASC;
 
-				if (last_d < 0)
+		if (last_d < 0)
 						{
 									last_d = -last_d;
 										}
 					_putchar(ASC = '0' + last_d);	/*converts to its ASCII value*/
 		return (last_d);
-}
