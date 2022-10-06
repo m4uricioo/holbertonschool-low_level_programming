79d009ade8630309a5dd6f9f7fa615c5ae34aae2
mauricio <imcorreamauricio@gmail.com>
2022-10-06 08:55:41 -0700

    tasks p and a

---
+++ pointers_arrays_strings/main.c
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*libreries*/
+#include <stdio.h>
+
+/*prototype*/
+
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+#endif /*main.h*/
