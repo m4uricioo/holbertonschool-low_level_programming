c4246cedcb528a3a4480207a5c94c8436f870087
mauricio <imcorreamauricio@gmail.com>
2022-10-07 05:34:07 -0700

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  swap_int -space for exchange
+ *  @a: its a value
+ * @b: its a value
+ */
+void swap_int(int *a, int *b)
+{
+	int valor;
+
+	temp = *a;
+	*a = *b;
+	*b = valor;
+}
