29a3e286e701e8a91c4770cf6e05aa505a0777d7
mauricio <imcorreamauricio@gmail.com>
2022-10-09 13:10:15 -0700

    task4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*  reverse_array - inverts the content of an array
+*   @ a: array
+*     @ n: numbers of elements of the array to swap
+**/
+
+void reverse_array(int *a, int n)
+{
+	int i, len;
+	int aux[999];
+
+	if (n <= 0)
+	;
+	else
+	{
+	for (i = 0; i < n; i++)
+	aux[i] = a[i];
+	for (i -= 1, len = i; i >= 0; i--)
+	a[len - i] = aux[i];
+}
+}
