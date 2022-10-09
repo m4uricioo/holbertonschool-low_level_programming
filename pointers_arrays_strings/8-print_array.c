03678dda9af41f0ef8ac8303eb1c72a5ec78b983
mauricio <imcorreamauricio@gmail.com>
2022-10-09 12:24:48 -0700

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -5,28 +5,28 @@
  *    */
 void puts_half(char *str)
 {
-		int len = _strlen(str) - 1;	/*string length minus the null charcater*/
-			int n = len / 2;	/*contains half of string*/
+	int len = _strlen(str) - 1;	/*string length minus the null charcater*/
+	int n = len / 2;	/*contains half of string*/
 
-				if (n % 2 == 0)		/*if the number is even*/
-						{
-									for (n++; str[n]; n++)	/*itera from n+1*/
-												{
-																_putchar(str[n]);
-																		}
-										}
-					_putchar('\n');
+	if (n % 2 == 0)		/*if the number is even*/
+{
+	for (n++; str[n]; n++)	/*itera from n+1*/
+{
+	_putchar(str[n]);
+}
+}
+	_putchar('\n');
 }
 /**
- *  * _strlen - returns the length of a string
- *   * @s: pinter to addres os strign
- *    * Return: string length
- *     */
+*  * _strlen - returns the length of a string
+*   * @s: pinter to addres os strign
+*    * Return: string length
+**/
 int _strlen(char *s)
 {
-		int count = 0;
+	int count = 0;
 
-			for (; s[count] != '\0'; count++)
+	for (; s[count] != '\0'; count++)
 						;
-				return (count);
+return (count);
 }
