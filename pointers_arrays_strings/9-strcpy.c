fe5cffc3a11b47e4dbb5a3e2dd3ae49d7c6f84ad
mauricio <imcorreamauricio@gmail.com>
2022-10-09 12:31:49 -0700

    task9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*  _strcpy -  function that copies the string
+*   * @src: pointer to the strign
+*    * @dest: string copy destination
+*     * Return: dest
+**/
+char *_strcpy(char *dest, char *src)
+{
+	char *copy = dest;	/*store the memory address of de dest in an auxiliary pointer*/
+
+	while ((*copy++ = *src++) != '\0')	/*copy the values pointed to by src into dest*/
+		;
+	return (dest);
+}
