1b8dd66b77555c6898b1ac775042b4469acf3cf1
mauricio <imcorreamauricio@gmail.com>
2022-10-13 08:09:02 -0700

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- *  * rev_string - reverses a string
- *   * @s: string to be reversed
- **/
+*  * rev_string - reverses a string
+*   * @s: string to be reversed
+**/
 
 void rev_string(char *s)
 {
-		char aux[999];
-			int i, size;
+	char aux[999];
+	int i, size;
 
-				for (i = 0; s[i]; i++)
-							aux[i] = s[i];
-					for (i -= 1, size = i; i + 1; i--)
-								s[size - i] = aux[i];
+	for (i = 0; s[i]; i++)
+	aux[i] = s[i];
+	for (i -= 1, size = i; i + 1; i--)
+	s[size - i] = aux[i];
 }
