1c34137a0af4c0d2e1c0940c1f393a598c272786
mauricio <imcorreamauricio@gmail.com>
2022-10-13 08:15:08 -0700

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
 *  * print_array - prints n elements of an array of integers
-*   *@a: array of inters *    *@n: number of elements of the array to be printed
+*   *@a: array of inters *    *@n: number of e
 **/
 void print_array(int *a, int n)
 {
 		int i;
 
-	for (i = 0; i < n; i++)		/*goes from index 0 to less than total number of element*/
+	for (i = 0; i < n; i++)		/*goes from indent*/
 		{
 	if (i == n - 1)		/*when i equals the last index*/
 {
