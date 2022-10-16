712e76390d634413119aaf0850c5f22dafcecafd
mauricio <imcorreamauricio@gmail.com>
2022-10-16 10:59:33 -0700

    main.h

--- recursion/main.h
+++ recursion/main.h
@@ -1,11 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
-/*libreries*/
-#include <unistd.h>
-/*prototypes*/
-
 int _putchar(char c);
-
+int aux(int num, int guess);
+int is_prime_number(int n);
+int extra(int num, int guess);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
-
-#endif/*main.h*
