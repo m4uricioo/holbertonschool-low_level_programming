eb55e19de3b1dc3aa78c78933671723c7abbf6b8
mauricio <imcorreamauricio@gmail.com>
2022-10-29 13:04:04 -0700

    pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/*ime - prints a name.
+  * @name: String which is the name.
+  * @f: Function pointer to uppercase name function or as passed name.
+  *
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
