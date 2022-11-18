efab87b8db03bdfba99f912f34f2f6f8bb05e991
mauricio <imcorreamauricio@gmail.com>
2022-11-18 15:11:18 -0800

    task5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,16 +1,20 @@
 #include "lists.h"
 
 /**
-* free_listint - free list
-*
-*@head: pointer to first node
-*/
-
-void free_listint(listint_t *head)
+ * free_listint2 - free a `listint_t` list
+ * @head: double pointer to head
+ */
+void free_listint2(listint_t **head)
 {
-	if (head)
+	listint_t *hold;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
 	{
-		free_listint(head->next);
-		free(head);
+		hold = *head;
+		*head = (*head)->next;
+		free(hold);
 	}
 }
